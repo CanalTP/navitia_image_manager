8257f47592db9b2c8560d30cfe96064793a2576b
François Vincent <francois.vincent01@gmail.com>
2015-11-19 17:17:14 +0100

    fix import, update artemis

--- factories/navitia_artemis.py
+++ factories/navitia_artemis.py
@@ -13,14 +13,13 @@ sys.path[0] = ROOT
 from clingon import clingon
 clingon.DEBUG = True
 
-from navitia_docker_manager import DIM, FFD, utils
+from navitia_image_manager import DIM, FFD, utils
 
 Dockerfile = """
 FROM navitia/debian8:latest
 
 RUN pip install pytest
 
-# mapped volumes (or not)
 RUN mkdir -m 777 -p /srv/ed/data
 
 COPY {supervisord_conf} /etc/supervisor/conf.d/supervisord.conf
@@ -32,19 +31,18 @@ FINAL_IMAGE_NAME = 'navitia/debian8_artemis'
 CONTAINER_NAME = 'artemis'
 
 
-@clingon.clize(artemis_data=('ad',), artemis_source=('as',), set_version=('s', 'v'))
-def factory(navitia_folder='',
-            data_folder='',
+@clingon.clize(artemis_source=('as',), artemis_data=('ad',), set_version=('v',))
+def factory(navitia_packages='',
             port='',
-            artemis_data='',
             artemis_source='',
+            artemis_data='',
             commit=False,
             remove=False,
             set_version='',
             tag=False):
     if commit:
         if set_version.lower() in ('true', 'yes'):
-            version = utils.get_packet_version(navitia_folder)
+            version = utils.get_packet_version(navitia_packages)
         elif not set_version or set_version.lower() in ('false', 'no'):
             version = None
         else:
@@ -54,11 +52,10 @@ def factory(navitia_folder='',
             print("image {} already exists".format(image_name))
             return 1
     drp = DIM.DockerRunParameters(
-        hostname='navitia',
-        volumes=(data_folder + ':/srv/ed/data',
-                 artemis_data + ':/srv/artemis/data',
-                 artemis_source + ':/srv/artemis/source'),
-        ports=(port + ':80',)
+        hostname='artemis',
+        volumes=(artemis_data + ':/artemis/data',
+                 artemis_source + ':/artemis/source'),
+        ports=(port + ':80', '9002:9001')
     )
     ffd = FFD.FabricForDocker(None, user='navitia', platform='artemis', distrib='debian8')
     df = DIM.DockerFile(
@@ -74,7 +71,7 @@ def factory(navitia_folder='',
         dcm = dim.create_container(CONTAINER_NAME, start=True, if_exist='remove')
         ffd.set_platform(dcm)
         time.sleep(5)
-        with utils.chdir(navitia_folder):
+        with utils.chdir(navitia_packages):
             ffd.execute('deploy_from_scratch')
         if commit:
             dcm.stop()
